d8007d160c487ffb1dbe197523f510937f216959
Michael <michaelconstantinou330@gmail.com>
2019-10-13 11:45:36 +0100

    created galleries model and relations to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_093245) do
+ActiveRecord::Schema.define(version: 2019_10_13_103519) do
+
+  create_table "galleries", force: :cascade do |t|
+    t.string "name"
+    t.string "cover_image"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
