9d03f0a0ee220a967978db2381c4ea188fceddb2
Michael <michaelconstantinou330@gmail.com>
2019-10-16 18:08:14 +0100

    Removed unnecessary views and configured user permissions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :galleries
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'galleries#index'
 end
